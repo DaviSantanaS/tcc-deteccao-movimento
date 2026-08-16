12977da749275eb8c703318e8d61b90108cbc651
Davi Santana <53800198+DaviSantanaS@users.noreply.github.com>
2026-08-16 18:07:43 -0300

    refactor mog2 stream reader interface

---
+++ motion_mog2/include/VideoStreamReader.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <opencv2/cudacodec.hpp>
+#include <opencv2/core/cuda.hpp>
+
+#include <chrono>
+#include <cstdint>
+#include <string>
+#include <vector>
+
+struct EncodedPacket {
+    std::vector<unsigned char> data;
+    std::chrono::steady_clock::time_point received_at;
+    bool has_key_frame = false;
+};
+
+struct VideoFrameData {
+    cv::cuda::GpuMat decoded_frame_gpu;
+    std::vector<EncodedPacket> encoded_packets;
+    uint64_t frame_index = 0;
+};
+
+class VideoStreamReader {
+public:
+    explicit VideoStreamReader(const std::string& rtsp_url);
+
+    bool read(VideoFrameData& frame_data, cv::cuda::Stream& cuda_stream);
+
+    double fps() const;
+    int width() const;
+    int height() const;
+    uint64_t processedFrameCount() const;
+
+private:
+    cv::Ptr<cv::cudacodec::VideoReader> video_reader_;
+    size_t decoded_frame_index_ = 0;
+    size_t raw_package_base_index_ = 0;
+
+    double stream_fps_ = 0.0;
+    int frame_width_ = 0;
+    int frame_height_ = 0;
+    uint64_t next_frame_index_ = 0;
+};
