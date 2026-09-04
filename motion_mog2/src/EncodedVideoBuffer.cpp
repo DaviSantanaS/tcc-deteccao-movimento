5f8846795020f6fdc9f1b2d187a1ef20bf71f220
Davi Santana <53800198+DaviSantanaS@users.noreply.github.com>
2026-09-04 16:09:59 -0300

    refactor: separa responsabilidades do startMotion

--- motion_mog2/src/EncodedVideoBuffer.cpp
+++ motion_mog2/src/EncodedVideoBuffer.cpp
@@ -33,27 +33,53 @@ MotionBufferStartInfo EncodedVideoBuffer::startMotion(
     uint64_t motion_decoded_frame_index,
     const std::vector<EncodedPacket>& current_encoded_packets
 ) {
+    resetMotionBuffer();
+
+    if (hasCurrentGopWithKeyFrame()) {
+        copyCurrentGopToMotionBuffer(motion_decoded_frame_index);
+    } else {
+        startMotionBufferFromCurrentPackets(
+            motion_decoded_frame_index,
+            current_encoded_packets
+        );
+    }
+
+    return buildMotionBufferStartInfo(motion_decoded_frame_index);
+}
+
+void EncodedVideoBuffer::resetMotionBuffer() {
     motion_encoded_packets_.clear();
     motion_encoded_byte_count_ = 0;
     motion_extra_decoded_frames_before_start_ = 0;
+}
 
+bool EncodedVideoBuffer::hasCurrentGopWithKeyFrame() const {
+    return current_gop_has_key_frame_ && !current_gop_encoded_packets_.empty();
+}
+
+void EncodedVideoBuffer::copyCurrentGopToMotionBuffer(
+    uint64_t motion_decoded_frame_index
+) {
+    motion_encoded_packets_ = current_gop_encoded_packets_;
+    motion_encoded_byte_count_ = current_gop_encoded_byte_count_;
+    motion_start_decoded_frame_index_ = current_gop_start_decoded_frame_index_;
+    motion_extra_decoded_frames_before_start_ =
+        motion_decoded_frame_index - current_gop_start_decoded_frame_index_;
+}
+
+void EncodedVideoBuffer::startMotionBufferFromCurrentPackets(
+    uint64_t motion_decoded_frame_index,
+    const std::vector<EncodedPacket>& current_encoded_packets
+) {
+    motion_start_decoded_frame_index_ = motion_decoded_frame_index;
+    appendMotionPackets(current_encoded_packets);
+}
+
+MotionBufferStartInfo EncodedVideoBuffer::buildMotionBufferStartInfo(
+    uint64_t motion_decoded_frame_index
+) const {
     MotionBufferStartInfo info;
     info.motion_decoded_frame_index = motion_decoded_frame_index;
-
-    if (current_gop_has_key_frame_ && !current_gop_encoded_packets_.empty()) {
-        motion_encoded_packets_ = current_gop_encoded_packets_;
-        motion_encoded_byte_count_ = current_gop_encoded_byte_count_;
-        motion_start_decoded_frame_index_ = current_gop_start_decoded_frame_index_;
-        motion_extra_decoded_frames_before_start_ =
-            motion_decoded_frame_index - current_gop_start_decoded_frame_index_;
-    } else {
-        motion_start_decoded_frame_index_ = motion_decoded_frame_index;
-        for (const EncodedPacket& encoded_packet : current_encoded_packets) {
-            motion_encoded_byte_count_ += encoded_packet.data.size();
-            motion_encoded_packets_.push_back(encoded_packet);
-        }
-    }
-
     info.start_decoded_frame_index = motion_start_decoded_frame_index_;
     info.extra_decoded_frames_before_motion =
         motion_extra_decoded_frames_before_start_;
